e0087b3d8ff704027b9c5ce66fe3b4bf34d59354
jjun0214z <jjun0214z@gmail.com>
2018-11-07 10:33:19 +0900

    .

--- jevtagram/images/admin.py
+++ jevtagram/images/admin.py
@@ -5,45 +5,45 @@ from . import models
 @admin.register(models.Image)
 class ImageAdmin(admin.ModelAdmin):
 
-    list_display_links = (
+    list_display_links = [
         'location',
-    )
+    ]
 
-    search_fields = (
+    search_fields = [
         'location',
         'caption',
-    )
+    ]
 
-    list_filter = (
+    list_filter = [
         'location',
-    )
+    ]
 
-    list_display = (
+    list_display = [
         'file',
         'location',
         'caption',
         'creator',
         'created_at',
         'updated_at',
-    )
+    ]
 
 @admin.register(models.Like)
 class LikeAdmin(admin.ModelAdmin):
 
-    list_display = (
+    list_display = [
         'creator',
         'image',
         'created_at',
         'updated_at',
-    )
+    ]
 
 @admin.register(models.Comment)
 class CommentAdmin(admin.ModelAdmin):
 
-    list_display = (
+    list_display = [
         'message',
         'creator',
         'image',
         'created_at',
         'updated_at',
-    )
+    ]
