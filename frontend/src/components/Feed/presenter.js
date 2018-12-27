e7e1683bf8f85814ae6d93097a50acd677b804af
jjun0214z <jjun0214z@gmail.com>
2018-12-27 10:26:03 +0900

    랜더링 Feed

--- frontend/src/components/Feed/presenter.js
+++ frontend/src/components/Feed/presenter.js
@@ -6,13 +6,21 @@ import Loading from "components/Loading";
 
 const Feed = (props, context) => {
     if ( props.loading ) {
-        return <LodingFeed />
+        return <LodingFeed />;
+    }else if( props.feed ){
+        return <RenderFeed {...props} />;
     }
 };
 
 const LodingFeed = (props, context) => (
     <div className={styles.feed}>
         <Loading />
+    </div>
+);
+
+const RenderFeed = (props, context) => (
+    <div className={styles.feed}>
+        {props.feed.map(post => post.caption)}
     </div>
 );
 
